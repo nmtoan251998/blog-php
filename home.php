02e4b7bad52009db868a4949a800ee6a19c3ce89
Noat <nmtoan251998@gmail.com>
2019-02-28 19:11:13 +0700

    Create PostPage

--- home.php
+++ home.php
@@ -23,6 +23,7 @@
                 if ($result->num_rows > 0) {
                     while($row = $result->fetch_assoc()) {
                         
+                        $blogId = $row["blog_id"];
                         $blogTitle = ucwords($row["blog_title"]);
                         $blogContent = substr($row["blog_description"], 0, 1000);                    
                         $blogAuthor = $row["blog_author"];                                        
@@ -31,16 +32,16 @@
                         
                         echo '
                             <section class="blog">
-                                <h2><a href="#" class="heading-2 heading-2-primary blog__title">'.$blogTitle.'</a></h2>
-                                <p class="blog__content">'.$blogContent.' ...........<a href="#" class="blog__read-more">Read more</a> </p>                        
+                                <h2><a href="post.php?blog_id='.$blogId.'" class="heading-2 heading-2-primary blog__title">'.$blogTitle.'</a></h2>
                                 <div class="blog__info">                            
                                     <a href="#" title="'.$blogAuthor.'" class="blog__author">'.$blogAuthor.'</a>,
-                                    <span class="blog__date">'.$blogDate.'</span>
-                                    <div class="blog__category">
-                                        <span class="blog__category-text">Tag: </span>  
-                                        <a href="#" class="blog__category-content">'.$blogCategory.'</a>
-                                    </div>                            
+                                    <span class="blog__date">'.$blogDate.'</span>                                                 
                                 </div>                        
+                                <p class="blog__content">'.$blogContent.' ...........<a href="post.php?blog_id='.$blogId.'" class="blog__read-more">Read more</a> </p>                                                        
+                                <div class="blog__category">
+                                    <span class="blog__category-text">Tag: </span>  
+                                    <a href="#" class="blog__category-content">'.$blogCategory.'</a>
+                                </div>               
                             </section>
                             <hr />
                             <br/>
@@ -54,33 +55,7 @@
             <!-- end [Blog] -->            
         </main>
         <!-- end [main] -->  
-        <!-- [right-sidebar] -->
-        <aside class="right-sidebar">        
-            <h2 class="heading-2">The Latest Posts</h2>
-            <hr/>
-            <?php
-                $sql = "SELECT * FROM blog_data ORDER BY blog_id DESC LIMIT 5";
-                $result = mysqli_query($connection, $sql);                
-
-                // if data exist in database
-                if($result->num_rows > 0) {
-                    while($row = $result->fetch_assoc()) {
-                        
-                        $blogTitle = ucwords($row["blog_title"]);
-
-                        echo '                
-                            <section class="latest-posts">
-                                <div class="latest-post">
-                                    <a class="latest-post-item" href="#">'.$blogTitle.'</a>                        
-                                </div>                               
-                            </section>
-                            ';
-                    }
-                }
-            
-            ?>
-        </aside>
-        <!-- end [right-sidebar] -->                       
+        <?php include "includes/sidebar.php" ?>                          
     </div>    
 </body>
 </html>
---
+++ post.php
@@ -0,0 +1,66 @@
+<?php include "includes/config.php" ?>
+
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">    
+
+    <link rel="stylesheet" type="text/css" href="public/stylesheet/css/home.css">
+
+    <title>Home page</title>    
+</head>
+<body>
+    <div class="container">
+        <!-- [main]-->
+        <main class="main">
+            <!-- [Blog]-->
+            <?php                
+                if(isset($_GET["blog_id"])) { // [if block 1]
+                    $curBlog = $_GET["blog_id"];
+
+                    $sql = "SELECT * FROM blog_data WHERE blog_id ='$curBlog'";
+                    $result = mysqli_query($connection, $sql);
+
+                    // if data exist in database
+                    if ($result->num_rows > 0) { // [if block 2]
+                        while($row = $result->fetch_assoc()) {
+                            
+                            $blogId = $row["blog_id"];
+                            $blogTitle = ucwords($row["blog_title"]);
+                            $blogContent = $row["blog_description"];                    
+                            $blogAuthor = $row["blog_author"];                                        
+                            $blogDate = $row["blog_date"];
+                            $blogCategory = $row["blog_category"];
+                            
+                            echo '
+                                <section class="blog">                                
+                                    <h2 class="heading-2 heading-2-primary blog__title">'.$blogTitle.'</h2>
+                                    <div class="blog__info">                            
+                                        <a href="#" title="'.$blogAuthor.'" class="blog__author">'.$blogAuthor.'</a>,
+                                        <span class="blog__date">'.$blogDate.'</span>                                        
+                                    </div>                        
+                                    <p class="blog__content">'.$blogContent.'</p>    
+                                    <div class="blog__category">
+                                        <span class="blog__category-text">Tag: </span>  
+                                        <a href="#" class="blog__category-content">'.$blogCategory.'</a>
+                                    </div>                                                                                    
+                                </section>                                
+                                ';                    
+                        }                
+                    } // [end if block 2]
+
+                } // [end if block 1]
+                else { 
+            ?> 
+                <script>window.location = "home.php" </script>        
+            <?php
+                } 
+            ?>
+            <!-- end [Blog] -->            
+        </main>
+        <!-- end [main] -->  
+        <?php include "includes/sidebar.php" ?>                          
+    </div>    
+</body>
+</html>
