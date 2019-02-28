efa3955f30f35b1df442b909c8c8af680ca3e3ac
Noat <nmtoan251998@gmail.com>
2019-02-28 18:42:15 +0700

    Split Sidebar layout into a single file

---
+++ includes/sidebar.php
@@ -0,0 +1,34 @@
+<html lang="en">
+<head>
+    <title>Sidebar Layout</title>
+</head>
+<body>
+    <!-- [right-sidebar] -->
+    <aside class="right-sidebar">        
+            <h2 class="heading-2">The Latest Posts</h2>
+            <hr/>
+            <?php
+                $sql = "SELECT * FROM blog_data ORDER BY blog_id DESC LIMIT 5";
+                $result = mysqli_query($connection, $sql);                
+
+                // if data exist in database
+                if($result->num_rows > 0) {
+                    while($row = $result->fetch_assoc()) {
+                        
+                        $blogTitle = ucwords($row["blog_title"]);
+
+                        echo '                
+                            <section class="latest-posts">
+                                <div class="latest-post">
+                                    <a class="latest-post-item" href="#">'.$blogTitle.'</a>                        
+                                </div>                               
+                            </section>
+                            ';
+                    }
+                }
+            
+            ?>
+        </aside>
+        <!-- end [right-sidebar] -->     
+</body>
+</html>
