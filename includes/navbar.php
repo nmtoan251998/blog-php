f50426e40a47055bb3fa79ed42b397fa2b077a60
Noat <nmtoan251998@gmail.com>
2019-02-28 22:42:51 +0700

    Query data for static navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -15,24 +15,33 @@
             </li>
             <li class="nav--list">
                 <a href="#" class="nav--item">Item 3</a>
-            </li>
+            </li>            
             <li class="nav--list dropdown">
                 <a href="#" class="nav--item">Category</a>
-                <ul class="dropdown--menu">
-                    <li class="dropdown--list">
-                        <a href="#" class="dropdown--item">Category 1</a>
-                    </li>
-                    <li class="dropdown--list">
-                        <a href="#" class="dropdown--item">Category 2</a>
-                    </li>
-                    <li class="dropdown--list">
-                        <a href="#" class="dropdown--item">Category 3</a>
-                    </li>
-                    <li class="dropdown--list">
-                        <a href="#" class="dropdown--item">More </a>
-                    </li>
-                </ul>
-            </li>                
+                    <ul class="dropdown--menu">
+                        <?php
+                            $sql = "SELECT * FROM categories LIMIT 2";
+                            $result = mysqli_query($connection, $sql);
+
+                            // if data exist in database
+                            if($result->num_rows > 0) {
+                                while($row = $result->fetch_assoc()) {
+                                    $catData = ucwords($row['cat_data']);
+
+                                    echo '                                                                                           
+                                        <li class="dropdown--list">
+                                            <a href="#" class="dropdown--item">'.$catData.'</a>
+                                        </li>                                                                                                                                                           
+                                    ';
+                                }
+                            }                    
+                        ?>
+                        <li class="dropdown--list">
+                            <a href="#" class="dropdown--item">More </a>
+                        </li>
+                    </ul>
+                </li>
+                            
         </ul>
     </nav>
     <!-- [end navbar]-->
