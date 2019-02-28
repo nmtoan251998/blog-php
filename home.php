fde49e8276c495de897df20cf5b2e4bb8f946ede
Noat <nmtoan251998@gmail.com>
2019-02-28 13:00:57 +0700

    Create markup - Get database for Homepage

---
+++ home.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">    
+    <title>Home page</title>    
+</head>
+<body>
+    <div class="container">
+        <!-- [main]-->
+        <main class="main">
+            <!-- [Blog]-->
+            <?php
+                $sql = "SELECT * FROM blog_data ORDER BY blog_id DESC LIMIT 3";
+                $result = mysqli_query($conn, $sql);
+                
+                // if data exist in database
+                if ($result->num_rows > 0) {
+                    while($row = $result->fetch_assoc()) {
+                        
+                        $blogTitle = ucwords($row["blog_title"]);
+                        $blogContent = substr($row["blog_description"], 0, 1000);                    
+                        $blogAuthor = $row["blog_author"];                                        
+                        $blogDate = $row["blog_date"];
+                        $blogCategory = $row["blog_category"];
+                        
+                        echo '
+                            <section class="blog">
+                                <h2><a href="#" class="heading-2 heading-2-primary blog__title">'.$blogTitle.'</a></h2>
+                                <p class="blog__content">'.$blogContent.' ...........<a href="#" class="blog__read-more">Read more</a> </p>                        
+                                <div class="blog__info">                            
+                                    <a href="#" title="'.$blogAuthor.'" class="blog__author">'.$blogAuthor.'</a>,
+                                    <span class="blog__date">'.$blogDate.'</span>
+                                    <div class="blog__category">
+                                        <span class="blog__category-text">Tag: </span>  
+                                        <a href="#" class="blog__category-content">'.$blogCategory.'</a>
+                                    </div>                            
+                                </div>                        
+                            </section>
+                            <hr />
+                            <br/>
+                            ';                    
+                    }                
+                }
+                else { 
+                    echo "No matching records are found."; 
+                } 
+            ?>
+            <!-- end [Blog] -->            
+        </main>
+        <!-- end [main] -->                     
+    </div>    
+</body>
+</html>
