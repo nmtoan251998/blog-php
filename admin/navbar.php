3abb45b0ad31534bafd0d8c3f8c66b67f91dffc7
Noat <nmtoan251998@gmail.com>
2019-03-03 10:13:26 +0700

    Create navbar for AdminPage

---
+++ admin/navbar.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Navbar Admin Layout</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+        <a href="index.php" class="navbar-brand text-light">Admin Panel</a>                  
+        <button class="navbar-toggler bg-light" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a href="#" class="nav-link text-white">Home</a>
+                </li>
+                <li class="nav-item active">
+                    <a href="#" class="nav-link text-white">Post</a>
+                </li>
+                <li class="nav-item active">
+                    <a href="#" class="nav-link text-white">Data</a>
+                </li>                    
+            </ul>
+            <form action="" class="form-inline">
+                <input type="search" class="form-control mr-sm-2" placeholder="Input something here" aria-label="Search" size="">
+                <button class="btn btn-light" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
+</body>
+</html>
